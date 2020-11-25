67dba3dbf9a56184c61783488de25e005b996131
Ryu Shinmura <rysestock@gmail.com>
2020-11-26 01:14:12 +0900

    SitePluginとmcv2Pluginの依存関係を逆にした
    Plugin->SitePluginだったのをSitePlugin->Pluginにした。SitePluginはPluginの一種という位置付けにしたかった。

--- SitePlugin/IMcvSitePlugin.cs
+++ SitePlugin/IMcvSitePlugin.cs
@@ -3,25 +3,6 @@ using System.Threading.Tasks;
 
 namespace SitePlugin
 {
-    public sealed class SitePluginId
-    {
-        private Guid _guid;
-        public SitePluginId(Guid guid)//起動する毎に値が変わると何かと不便そうだから固定値を代入する形にした
-        {
-            _guid = guid;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            return obj is SitePluginId id &&
-                   _guid.Equals(id._guid);
-        }
-
-        public override int GetHashCode()
-        {
-            return -2045414129 + _guid.GetHashCode();
-        }
-    }
     public sealed class CommentProviderId
     {
         private Guid _guid;
